5bec042c44a82ce628750992c7eed6b5ed9c6087
Luis Heli <luiszouzsa@gmail.com>
2022-11-02 12:44:49 -0300

    update

---
+++ executar.js
@@ -0,0 +1,22 @@
+function criaraluno(nome, n1, n2) {
+    return {
+        nome: nome,
+        nota1: n1,
+        nota2: n2,
+        media: function(){
+        return (this.nota1 + this.nota2)/2;
+        }
+    }
+}
+
+var turma = [
+    criaraluno("pedro", 7, 9),
+    criaraluno("carlos", 5, 4),
+]
+
+var aluno = turma[0]
+var aluno1 = turma[1]
+
+turma.forEach(function(elemento) {
+    console.log(aluno)
+}) 
--- overview.js
+++ overview.js
@@ -1,6 +1,8 @@
-var alunos = ["pedro", "carlos", "antonio"]
-var notasa = [8, 5, 7]
-var notasb = [9, 3, 4]
+/* media*/
+
+var alunos = ["pedro", "carlos", "arthur"]
+var notaa = [8, 7, 3]
+var notab = [9, 6, 4]
 
 function media(n1, n2) {
     var media = (n1+n2)/2
@@ -8,20 +10,71 @@ function media(n1, n2) {
 }
 
 function resultado(media) {
-    if(media > 6) {
+    if(m > 6) {
         return "aprovado"
     }
     else {
-        return "reprovado"
+        return "desaprovado"
     }
 }
 
-for (var index in alunos) {
+for(var index in alunos) {
 
-    var n1 = notasa[index]
-    var n2 = notasb[index]
-    var m = media(n1, n2)
-    
-    console.log(alunos[index] + " - " + notasa[index] + " - " + notasb[index] + " - " + media(notasa[index], notasb[index]) + " - " + 
-    resultado(m))
+    var m = media(notaa[index], notab[index]) 
+
+    console.log(alunos[index] + " - " + notaa[index] + " - " + notab[index] + " - " + m + " - " + resultado())
 }
+
+console.log("--------")
+
+/*object*/
+
+var aluno = {nome: "pedro", nota: 9}
+console.log(aluno.nome) //da pra ser aluno[nome]
+
+//array dentro do object
+
+var aluno = {nome: "pedro", notas: [8, 7]}
+console.log(aluno.notas[0])
+
+//adicionar property em um object
+
+aluno.matricula = 4235
+console.log(aluno.matricula)
+
+//adicionar property em um object, em forma de var
+
+var outravariavel = "sobrenome"
+aluno[outravariavel] = "henrique"
+console.log(aluno) //vai estar sobrenome, no meio do object
+
+
+console.log("------------------")
+
+
+//outra forma de fazer um object
+
+var aluno2 = new Object
+
+aluno2.nome = "carlos"
+aluno2.notas = [8, 7]
+
+console.log(aluno2)
+
+
+
+console.log("--------")
+//exemplo de metodo no obejct
+
+function calcmedia () {
+    return (this.notas[0] + this.notas[1])/2
+}
+
+var aluno = {nome: "pedro", notas: [8, 7], 
+             media: calcmedia
+}
+
+var aluno1 = {nome: "carlos", notas: [9, 8], media: calcmedia}
+
+console.log(aluno.nome + " - " + aluno.notas[0] + " - " + aluno.notas[1] + " - " + aluno.media())
+console.log(aluno1.nome + " - " + aluno1.notas[0] + " - " + aluno1.notas[1] + " - " + aluno1.media())
