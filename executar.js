ca81f4b338f604d1a15047509bfa0fa32c907553
Luis Heli <luiszouzsa@gmail.com>
2022-11-27 15:26:25 -0300

    update1

--- executar.js
+++ executar.js
@@ -1,22 +1,12 @@
-function criaraluno(nome, n1, n2) {
-    return {
-        nome: nome,
-        nota1: n1,
-        nota2: n2,
-        media: function(){
-        return (this.nota1 + this.nota2)/2;
-        }
-    }
+var calcmedia = function() {
+    return (this.nota1 + this.nota2)/2
 }
 
 var turma = [
-    criaraluno("pedro", 7, 9),
-    criaraluno("carlos", 5, 4),
+    {
+        nome: "Pedro", nota1: 9, nota2: 8, media:calcmedia
+    }
 ]
 
-var aluno = turma[0]
-var aluno1 = turma[1]
-
-turma.forEach(function(elemento) {
-    console.log(aluno)
-})
+console.log(turma[0])
+console.log(turma[0].media())
